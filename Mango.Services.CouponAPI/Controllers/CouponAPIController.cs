8175a0b3d9b620d51f32f05b5af431e9698980a0
Taha <taha100_100@yahoo.com>
2024-08-02 23:12:20 +0300

    Add CouponAPI controller with two get actions

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models.Dto;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -7,5 +11,46 @@ namespace Mango.Services.CouponAPI.Controllers
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponId == id);
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
