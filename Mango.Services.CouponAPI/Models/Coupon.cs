7e8c1919fccee7afcfff682262bcc7e48fb4e519
Taha <taha100_100@yahoo.com>
2024-08-02 22:43:14 +0300

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAMount { get; set; }
+        public double MinAmount { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAMount { get; set; }
+        public double MinAmount { get; set; }
+    }
+}
