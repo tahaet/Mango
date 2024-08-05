0b4b6c95bac56bbd60cd38d3540c7aac93b7f1ce
Taha <taha100_100@yahoo.com>
2024-08-05 17:41:25 +0300

    add ApiType enum in Mango.Web.Utility SD class

--- Mango.Web/Models/RequestDto.cs
+++ Mango.Web/Models/RequestDto.cs
@@ -1,8 +1,10 @@
-﻿namespace Mango.Web.Models
+﻿using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
 {
     public class RequestDto
     {
-        public string ApiType { get; set; } = "GET";
+        public  ApiType ApiType { get; set; } = ApiType.GET;
         public string Url { get; set; }
         public string Data { get; set; }
         public string AccesssToken { get; set; }
