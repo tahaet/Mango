142ed494ab6761011aa99a2d7c93c3aefa6cd676
Taha <taha100_100@yahoo.com>
2024-08-05 17:37:02 +0300

    add request and response Dtos in Mange.Web.Models

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public string ApiType { get; set; } = "GET";
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public string AccesssToken { get; set; }
+    }
+}
