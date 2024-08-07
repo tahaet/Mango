0b68bea5cfd7d8eea7ca4d889f02bac7675f40ae
Taha <taha100_100@yahoo.com>
2024-08-07 18:14:51 +0300

    add CouponCreate post action method in CouponController

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -27,5 +27,19 @@ namespace Mango.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
     }
 }
