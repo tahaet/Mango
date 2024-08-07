4b883031141b529629bbae451b11b0ea986e5169
Taha <taha100_100@yahoo.com>
2024-08-07 15:55:45 +0300

    change string type to obbject in RequestDto

--- Mango.Web/Service/IService/CouponService.cs
+++ Mango.Web/Service/IService/CouponService.cs
@@ -1,35 +1,43 @@
 ﻿using Mango.Web.Models;
+using Mango.Web.Utility;
+using static Mango.Web.Utility.SD;
 
 namespace Mango.Web.Service.IService
 {
     public class CouponService : ICouponService
     {
-        public Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto)
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto)
+        {
+           throw new NotImplementedException();
+        }
+
+        public async Task<ResponseDto?> DeleteCouponsAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ResponseDto?> DeleteCouponsAsync(int id)
+        public async Task<ResponseDto?> GetAllCouponsAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<ResponseDto?> GetAllCouponsAsync()
+        public async Task<ResponseDto?> GetCouponAsync(string couponCode)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ResponseDto?> GetCouponAsync(string couponCode)
+        public async Task<ResponseDto?> GetCouponByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ResponseDto?> GetCouponByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
+        public async Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
         {
             throw new NotImplementedException();
         }
