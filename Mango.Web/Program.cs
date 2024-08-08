9273bf72da7f9171affcab564a00a6ef5e545b38
Taha <taha100_100@yahoo.com>
2024-08-09 01:10:16 +0300

    add Auth service in Mango.Web

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -8,10 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddHttpClient<ICouponService,ICouponService>();
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"]!;
 SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"]!;
 builder.Services.AddScoped<IBaseService, BaseService>();   
+builder.Services.AddScoped<IAuthService, AuthService>();   
 builder.Services.AddScoped<ICouponService, CouponService>();   
 var app = builder.Build();
 
