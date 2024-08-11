beda7ac0655d385123986bf67f6e8d5938d7687d
Taha <taha100_100@yahoo.com>
2024-08-11 23:47:05 +0300

    complete GetCart action method in Mango.Services.ShoppingCartAPI

--- Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using Azure;
 using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,11 +17,13 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private ResponseDto _response;
         private readonly AppDbContext _db;
         private readonly IMapper _mapper;
+        private readonly IProductService _productService;
 
-        public CartController(AppDbContext db,IMapper mapper)
+        public CartController(AppDbContext db,IMapper mapper,IProductService productService)
         {
             _db = db;
             _mapper = mapper;
+            _productService = productService;
         }
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert([FromBody] CartDto cartDto)
@@ -113,9 +116,10 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails
                     .Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId));
 
-
+                IEnumerable<ProductDto> productsDto = await _productService.GetProducts();
                 foreach (var item in cart.CartDetails)
                 {
+                    item.Product = productsDto.FirstOrDefault(x => x.ProductId == item.ProductId);
                     cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
                 }
 
