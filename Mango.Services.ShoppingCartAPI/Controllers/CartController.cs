a68f398c36dcd7e43481a94d11107d117e4b150f
Taha <taha100_100@yahoo.com>
2024-08-11 16:26:23 +0300

    add CartController with empty Upsert post action method in Mango.Services.ShoppingCartAPI

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private ResponseDto _responseDto;
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CartController(AppDbContext db,IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> CartUpsert([FromBody] CartDto cartDto)
+        {
+
+        }
+    }
+}
