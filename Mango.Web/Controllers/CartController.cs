ee27ecaba0410d49871deac372d87020ea5868b7
Taha <taha100_100@yahoo.com>
2024-08-12 14:37:44 +0300

    add CartController in Mango.Web with CartIndex action method and navigation in _Layout.cshtml

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        public async Task<IActionResult> CartIndex()
+        {
+            return View( await LoadCartDtoBasednLoggedInUser());
+        }
+        private async Task<CartDto?> LoadCartDtoBasednLoggedInUser()
+        {
+            var userId = User.Claims.Where(x => x.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDto? response = await _cartService.GetCartByUserIdAsnyc(userId);
+            if( response != null && response.IsSuccess )
+            {
+                return JsonConvert.DeserializeObject<CartDto?>(Convert.ToString(response.Result)!);
+            }
+            return new CartDto();
+        }
+    }
+}
