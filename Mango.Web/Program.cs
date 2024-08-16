16fe6c0d2ca58e7e15a1c40130998fb659cdc56e
Taha <taha100_100@yahoo.com>
2024-08-17 00:51:14 +0300

    register OrderService in program.cs with needed configurations

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<IProductService,ProductService>();
 builder.Services.AddHttpClient<ICartService,CartService>();
+builder.Services.AddHttpClient<IOrderService,OrderService>();
 builder.Services.AddHttpClient<ICouponService,CouponService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"]!;
@@ -22,6 +23,7 @@ builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<IBaseService, BaseService>();   
 builder.Services.AddScoped<IAuthService, AuthService>();   
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
