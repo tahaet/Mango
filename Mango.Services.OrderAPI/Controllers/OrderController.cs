be9e1bda66ca180243ee2e7135543d8cade5daa3
Taha <taha100_100@yahoo.com>
2024-08-17 00:40:19 +0300

    add CreateOrder post action methos in Mango.Services.OrderAPI

--- Mango.Services.OrderAPI/Controllers/OrderController.cs
+++ Mango.Services.OrderAPI/Controllers/OrderController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using Mango.MessageBus;
 using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models;
 using Mango.Services.OrderAPI.Models.Dto;
 using Mango.Services.OrderAPI.Service.IService;
+using Mango.Services.OrderAPI.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -23,6 +26,31 @@ namespace Mango.Services.OrderAPI.Controllers
             this._response = new ResponseDto();
             _productService = productService;
             _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpPost("CreateOrder")]
+        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = SD.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+                
+                OrderHeader orderCreated = _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _db.SaveChangesAsync();
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+                _response.Result = orderHeaderDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
         }
     }
 }
