efaf25478c6e53070cd84ab89d5fbb36bb394e07
Taha <taha100_100@yahoo.com>
2024-08-17 00:11:04 +0300

    create maps configs for AUtoMapper

--- Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
 
 
 namespace Mango.Services.OrderAPI
@@ -9,7 +11,13 @@ namespace Mango.Services.OrderAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-               
+                config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+                config.CreateMap<OrderDetails, OrderDetailsDto>().ReverseMap(); 
+                config.CreateMap<OrderHeaderDto,CartHeaderDto>().ForMember(dest=>dest.CartTotal, u=>u.MapFrom(src=>src.OrderTotal)).ReverseMap();
+                config.CreateMap<CartDetailsDto, OrderDetailsDto>().ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                .ForMember(des => des.Price, u => u.MapFrom(src => src.Product.Price));
+                config.CreateMap<OrderDetailsDto, CartDetailsDto>();
+
             });
             return mappingConfig;
         }
