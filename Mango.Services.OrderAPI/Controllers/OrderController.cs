f2b26a364c39b5546aa25645e01fc5dc6e428ab9
Taha <taha100_100@yahoo.com>
2024-08-17 00:27:17 +0300

    add OrderController in Mango.Services.OrderAPI

---
+++ Mango.Services.OrderAPI/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Mango.MessageBus;
+using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models.Dto;
+using Mango.Services.OrderAPI.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Mango.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        private readonly IProductService _productService;
+        public OrderAPIController(AppDbContext db,
+            IProductService productService, IMapper mapper)
+        {
+            _db = db;
+            this._response = new ResponseDto();
+            _productService = productService;
+            _mapper = mapper;
+        }
+    }
+}
