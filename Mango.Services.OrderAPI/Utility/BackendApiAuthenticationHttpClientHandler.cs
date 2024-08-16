64360e13af307b60c7f5970f8ac1d70b93369e9e
Taha <taha100_100@yahoo.com>
2024-08-16 23:15:32 +0300

    add inital Services in AMngo.Services.OrderAPI

---
+++ Mango.Services.OrderAPI/Utility/BackendApiAuthenticationHttpClientHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+
+namespace Mango.Services.OrderAPI.Utility
+{
+    public class BackendApiAuthenticationHttpClientHandler:DelegatingHandler
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public BackendApiAuthenticationHttpClientHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await httpContextAccessor.HttpContext.GetTokenAsync("access_token");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
