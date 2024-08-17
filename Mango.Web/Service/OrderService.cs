b42a740bcee8dbb9582262f6778754bb4818df33
Taha <taha100_100@yahoo.com>
2024-08-17 23:51:39 +0300

    add CreatseStripeSession service in Mango.Web

--- Mango.Web/Service/OrderService.cs
+++ Mango.Web/Service/OrderService.cs
@@ -22,5 +22,15 @@ namespace Mango.Web.Service
                 Url = SD.OrderAPIBase + "/api/order/CreateOrder"
             });
         }
+
+        public async Task<ResponseDto?> CreateStripeSession(StripeRequestDto stripeRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = stripeRequestDto,
+                Url = SD.OrderAPIBase + "/api/order/CreateStripeSession"
+            });
+        }
     }
 }
