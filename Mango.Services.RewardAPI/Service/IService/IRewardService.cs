1841ef88773e5724306578c9768657ffdedd6f4b
Taha <taha100_100@yahoo.com>
2024-08-18 23:54:57 +0300

    add RewardService in Mango.Services.RewardAPI

---
+++ Mango.Services.RewardAPI/Service/IService/IRewardService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.RewardAPI.Message;
+
+namespace Mango.Services.RewardAPI.Service.IService
+{
+    
+    public interface IRewardService
+    {
+        Task UpdateRewards(RewardsMessage rewardsMessage);
+    }
+    
+}
---
+++ Mango.Services.RewardAPI/Service/RewardService.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Services.RewardAPI.Data;
+using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Models;
+using Mango.Services.RewardAPI.Service.IService;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.RewardAPI.Service
+{
+    public class RewardService : IRewardService
+    {
+        private DbContextOptions<AppDbContext> _dbOptions;
+
+        public RewardService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            _dbOptions = dbOptions;
+        }
+
+        public async Task UpdateRewards(RewardsMessage rewardsMessage)
+        {
+            try
+            {
+                Reward rewards = new()
+                {
+                    OrderId = rewardsMessage.OrderId,
+                    RewardsActivity = rewardsMessage.RewardsActivity,
+                    UserId = rewardsMessage.UserId,
+                    RewardsDate = DateTime.Now
+                };
+                await using var _db = new AppDbContext(_dbOptions);
+                await _db.Rewards.AddAsync(rewards);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
