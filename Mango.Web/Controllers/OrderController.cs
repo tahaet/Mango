74b98bde08ba44daf8544dc0176ad57b70836b4f
Taha <taha100_100@yahoo.com>
2024-08-19 23:00:20 +0300

    add OrderController with Index action in Mango.Web

---
+++ Mango.Web/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [Authorize]
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+    }
+}
