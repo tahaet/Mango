cc58c9c2eae85354a779d4c5b590467917774823
Taha <taha100_100@yahoo.com>
2024-08-21 01:19:07 +0300

    add extensions and max file size validations to IFormFile property in Mango.Web models

---
+++ Mango.Web/Utility/MaxFileSizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Utility
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int maxSize;
+
+        public MaxFileSizeAttribute(int maxSize)
+        {
+            this.maxSize = maxSize;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > (maxSize * 1024 * 1024))
+                {
+                    return new ValidationResult($"Maximum allowed file size is {maxSize} MB.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
