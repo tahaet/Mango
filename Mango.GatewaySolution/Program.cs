e9917818317b518d051fd3520ec8babee36ee3e6
Taha <taha100_100@yahoo.com>
2024-08-21 15:39:34 +0300

    refactor Gateway Ocelot package configurations

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -29,10 +29,11 @@ builder.Services.AddAuthentication(x =>
         ValidateAudience = true
     };
 });
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json",optional: false,reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
